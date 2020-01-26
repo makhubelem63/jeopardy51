f54206dd7858f9b7c842693df9775adff75f2d4b
Brian Yu <brianyu28@gmail.com>
2020-01-26 02:33:26 -0500

    Updates

---
+++ precache-manifest.1fe0dd39d6ee042cf2f82723d64b718d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be47d18b56f0e00ee2e9ecc070ee7cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cefadd00a4b0d53f9b89",
+    "url": "/static/css/main.55ef191f.chunk.css"
+  },
+  {
+    "revision": "f32cdc05f8fbc17d3072",
+    "url": "/static/js/2.a08294c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a08294c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cefadd00a4b0d53f9b89",
+    "url": "/static/js/main.0efdd26b.chunk.js"
+  },
+  {
+    "revision": "f931408f4963c4212b83",
+    "url": "/static/js/runtime-main.39aa0b3d.js"
+  },
+  {
+    "revision": "6433bfa5b068274f1392b5301958e8e2",
+    "url": "/static/media/Swiss921-BT_43276.6433bfa5.ttf"
+  },
+  {
+    "revision": "64dc7745376af291834c6a14161c32e7",
+    "url": "/static/media/korinna.64dc7745.ttf"
+  }
+]);
